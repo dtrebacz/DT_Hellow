1fe646df852487513c8154f44bcfa370a266811a
dtrebacz <takijeden@autograf.pl>
2018-12-02 09:19:08 +0100

    Sting tokenizer

---
+++ src/TokenTester.java
@@ -0,0 +1,33 @@
+import java.util.StringTokenizer;
+
+public class TokenTester {
+
+    public static void main(String[] args) {
+        StringTokenizer st1, st2;
+
+
+        String quote1 = " GOOG 530,80 -9,98";
+        System.out.println("quotel: "+ quote1);
+
+        st1 = new StringTokenizer(quote1);
+
+        System.out.println("Token1:" + st1.nextToken());
+        System.out.println("Token1:" + st1.nextToken());
+        System.out.println("Token1:" + st1.nextToken());
+
+        System.out.println("");
+
+        String quote2 = "RTH@75,00@0,22";
+        st2 = new StringTokenizer(quote2,"@");
+
+        System.out.println("Token2:" + st2.nextToken());
+        System.out.println("Token2:" + st2.nextToken());
+        System.out.println("Token2:" + st2.nextToken());
+
+
+
+
+    }
+
+
+}
