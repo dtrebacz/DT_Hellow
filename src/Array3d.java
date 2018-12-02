278ef0e72e74a55d7f68619cf761c428d5ea9c99
dtrebacz <takijeden@autograf.pl>
2018-12-02 17:00:48 +0100

    Point

--- src/Array3d.java
+++ src/Array3d.java
@@ -2,25 +2,23 @@ public class Array3d {
 
     public static void main(String[] args) {
 
-        int[][][] array = { {{1},{1,2,3}},{{1,2},{3}}};
+        int[][][] array = { {{1},{1,2,3}},{{1,2},{3}},{{2,5,3}}};
 
         //System.out.println(array[1][2][0]);
 
       for(int a=0;a < array.length;a++)
       {
+          System.out.println("###########################################");
       for (int b=0;b < array[a].length ; b++)
       {
+          System.out.println();
       for (int c=0;c< array[a][b].length;c++) {
 
           System.out.print("array[" + a + "][" + b + "][" + c + "] =>" + array[a][b][c] +"\n");
       }
+      } System.out.println();
 
-
-      }
-
-
-
-      }
+      } System.out.println("###########################################");
 
 
 
