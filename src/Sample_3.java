182d4d65540110bf5982af0e7f40ab640b8d4c77
dtrebacz <takijeden@autograf.pl>
2018-12-02 10:48:15 +0100

    Point

--- src/Sample_3.java
+++ src/Sample_3.java
@@ -3,14 +3,31 @@ public class Sample_3 {
 
     public static void main(String[] args) {
 
-        byte a = 7;
+       /* byte a = 7;
         short b;
         int c;
 
         c = a;  //int miesci byte wiec konwersja moze sie odbyc niejawnie na poziomie kompilatora
 
-        b = (short)c;
+        b = (short)c;*/
+
+     /*  int a = 5;
+       double b = 13.5;
+       int c = (int)b/a;
+       int d = (int)b%a;
+*/
 
 
-    }
+     Employee emp = new Employee();
+     Vicepresident vp = new Vicepresident();
+
+     emp = vp;
+     vp = (Vicepresident)emp;
+
+        }
+
 }
+
+
+class Employee{}
+class Vicepresident extends Employee{}
