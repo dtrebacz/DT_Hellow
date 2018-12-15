e7848149be10506bd3cc8288187bba03ffead791
dtrebacz <takijeden@autograf.pl>
2018-12-15 10:57:23 +0100

    1

---
+++ src/Sample_6.java
@@ -0,0 +1,33 @@
+public class Sample_6 {
+
+
+
+    public static void main(String[] args) {
+        new A();
+        new A(1);
+        new A("ddd");
+    }
+
+
+}
+
+//*******************************************************************
+class A{
+    // Klasa a ma 3 konstruktory z różnymi argumentami
+    A(){
+        System.out.println("A: konstruktor bezargumentowy...");
+    }
+    A(int i){
+        System.out.println("A: konstruktor ze zmienną int...");
+    }
+    A(String S){
+        System.out.println("A: konstruktor ze zmienną String...");
+    }
+
+}
+
+
+class B{
+
+
+}
