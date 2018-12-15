dd292583d5c9640b3046939505737d4135a173e8
dtrebacz <takijeden@autograf.pl>
2018-12-15 11:23:06 +0100

    1

--- src/SubPrinter.java
+++ src/SubPrinter.java
@@ -2,9 +2,22 @@ public class SubPrinter extends Printer {
     int z = 3;
 
     public static void main(String[] args) {
+
+        Printer Printer = new Printer();
+        Printer.printMe();
+        System.out.println();
+
         SubPrinter subPrinter = new SubPrinter();
         subPrinter.printMe();
+        System.out.println();
+
     }
 
+
+    void printMe(){
+        super.printMe(); //wywolanie metody rodzica
+        System.out.println("z="+z);
+        System.out.println("jestem egzemplarzem klasy" + this.getClass().getName());
+    }
 
 }
